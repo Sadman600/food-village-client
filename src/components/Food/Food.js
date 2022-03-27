f74f75f226321eb500196c2a79da1ec18951e172
Sadman600 <sadmansakib600@gmail.com>
2022-03-27 12:48:41 +0600

    Load data in main componant and send data food componant

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Food.css';
-const Food = () => {
+const Food = props => {
+    const { image, name, price } = props.food;
     return (
-        <div>
-
+        <div className='food'>
+            <img src={image} alt='' ></img>
+            <div className='product-info'>
+                <h3>{name}</h3>
+                <h4>Price: ${price}</h4>
+            </div>
         </div>
     );
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Food from '../Food/Food';
 import './Main.css';
 
 const Main = () => {
-
+    const [foods, setFoods] = useState([]);
+    useEffect(() => {
+        fetch('foods.json')
+            .then(res => res.json())
+            .then(data => setFoods(data));
+    }, []);
     return (
         <div className='foods-container-row'>
             <div className='left-side-column'>
                 <div className='foods'>
-
+                    {
+                        foods.map(food => <Food key={food.id} food={food}></Food>)
+                    }
                 </div>
             </div>
             <div className='right-side-column'>gg</div>
