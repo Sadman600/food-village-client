668df6cfdd26c45537f3670bc5779d3bb27acfca
Sadman600 <sadmansakib600@gmail.com>
2022-03-27 12:31:25 +0600

    Added main component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+    return (
+        <div className='foods-container-row'>
+            <div className='left-side-column'>
+                <div className='foods'></div>
+            </div>
+            <div className='right-side-column'>gg</div>
+        </div>
+    );
+};
+
+export default Main;
